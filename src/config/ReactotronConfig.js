d2d0151f3628ec1d5187971081a0c51b3ce869c6
Bryan Alexandre <dkme01@gmail.com>
2019-09-30 16:14:20 -0300

    Icon import and style in the main page, needs fix

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({host: '127.0.0.1'})
+  const tron = Reactotron.configure({ host: '127.0.0.1' })
     .useReactNative()
     .connect();
 
