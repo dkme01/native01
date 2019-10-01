8e2f71521fa123ee909abb998c51a4e1b420921f
Bryan Alexandre <dkme01@gmail.com>
2019-10-01 18:48:40 -0300

    App now brings info from github api

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,4 +1,5 @@
 import Reactotron from 'reactotron-react-native';
+// import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
   const tron = Reactotron.configure({ host: '127.0.0.1' })
